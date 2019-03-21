86d510286bbc0705e80e8b4163e3ed97760dbdb7
aluno <aluno@localhost>
2019-03-21 15:52:52 -0300

    Operacao conta main

--- Banco/src/Conta.java
+++ Banco/src/Conta.java
@@ -1,4 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Conta {
-
+	private int numero;
+	private float saldo;
+	private List<Operacao> operacoes;
+	
+	
+	
+	
+	public Conta(int numero, float saldo) {
+		this.operacoes = new ArrayList<>();
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	
+	public void saque(Operacao op){
+		
+	}
+	public void deposito(Operacao op){
+		
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	
+	public float getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+	@Override
+	public String toString() {
+		return "Conta [numero=" + numero + ", saldo=" + saldo + "]";
+	}
+	
+	
+	
 }
--- Banco/src/Operacao.java
+++ Banco/src/Operacao.java
@@ -1,4 +1,49 @@
 
 public class Operacao {
+	
+	private TipoOperacao tipo;
+	private float valor;
+	
+	
+	
+	
+	public Operacao(TipoOperacao tipo, float valor) {
+		this.tipo = tipo;
+		this.valor = valor;
+	}
 
+
+
+
+	public TipoOperacao getTipo() {
+		return tipo;
+	}
+
+
+
+
+	public void setTipo(TipoOperacao tipo) {
+		this.tipo = tipo;
+	}
+
+
+
+
+	public float getValor() {
+		return valor;
+	}
+
+
+
+
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+
+
+
+
+	enum TipoOperacao{
+		SAQUE, DEPOSITO
+	}
 }
